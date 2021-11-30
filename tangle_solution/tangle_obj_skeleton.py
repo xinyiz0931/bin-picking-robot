de16536f44d7aa56fee1d1385e50c47bb3812efd
Xinyi Zhang <stannewone@gmail.com>
2021-11-30 20:51:03 +0900

    update

--- tangle_solution/tangle_obj_skeleton.py
+++ tangle_solution/tangle_obj_skeleton.py
@@ -1,3 +1,14 @@
+"""
+A python scripts to generate collision files for simulation models
+Arthor: Xinyi
+Date: 2021/10/15
+---
+Input: thinned skeleton of simulation model
+Output: decomposed cubes for collision checking
+---
+Be careful when the position of each cube are not change, 
+but the size of each cube are 1/2 of the original size. 
+"""
 import os
 import sys
 sys.path.append("./")
@@ -52,6 +63,72 @@ class TangleObjSke(object):
             vertice_sum += node[j]
         return vertice_sum / (edge_num*2)
     
+    # def calc_center_of_mass(self, vertices):
+    #     """
+    #     vertices: shape=(N,8,3)
+    #             N is the number of fitted cube, 8 is the number of vertices for cubes
+    #     """
+    #     vertices = np.array(vertices)
+    #     print(vertices.shape)
+    #     vnum = vertices.shape[0]*vertices.shape[1]
+    #     vpoints = np.reshape(vertices, (vnum, 3))
+
+    #     vsum = np.zeros(3)
+    #     for p in vpoints:
+    #         vsum += p
+    #     return vsum/vnum
+
+    def write_decomposed_obj(self, vertices, wrl_path):
+        """
+        vertices: shape=(N,8,3)
+                N is the number of fitted cube, 8 is the number of vertices for cubes
+        """
+        vertices = np.array(vertices)
+        vnum = vertices.shape[0]*vertices.shape[1]
+        fp = open(wrl_path, 'wt')
+        for v in vertices:
+            print("#VRML V2.0 utf8\n", file=fp)
+            print("Group {", file=fp)
+            print("	children [", file=fp)
+            print("		Shape {", file=fp)
+            print("			appearance Appearance {", file=fp)
+            print("				material Material {", file=fp)
+            print("					diffuseColor 0.410000 0.670000 0.340000", file=fp)
+            print("					ambientIntensity 0.400000", file=fp)
+            print("					specularColor 0.500000 0.500000 0.500000", file=fp)
+            print("					emissiveColor 0.000000 0.000000 0.000000", file=fp)
+            print("					shininess 0.400000", file=fp)
+            print("					transparency 0.000000", file=fp)
+            print("				} ", file=fp)
+            print("			} ", file=fp)
+            print("			geometry IndexedFaceSet {", file=fp)
+            print("				ccw TRUE", file=fp)
+            print("				solid TRUE", file=fp)
+            print("				convex TRUE", file=fp)
+            print("				coord DEF co Coordinate {", file=fp)
+            print("					point [", file=fp)
+            for i in range(8):
+                print(f"						{v[i][0]} {v[i][1]} {v[i][2]}", file=fp)
+
+            print("					]", file=fp)
+            print("				}", file=fp)
+            print("				coordIndex [ ", file=fp)
+            print("              0,  3,  2,  1, -1,", file=fp)
+            print("              4,  5,  6,  7, -1,", file=fp)
+            print("              0,  1,  5,  4, -1,", file=fp)
+            print("              3,  0,  4,  7, -1,", file=fp)
+            print("              2,  3,  7,  6, -1,", file=fp)
+            print("              1,  2,  6,  5, -1,", file=fp)
+            print("				]", file=fp)
+            print("			}", file=fp)
+            print("		}", file=fp)
+            print("	]", file=fp)
+            print("}\n", file=fp)
+
+        fp.close()
+        main_proc_print("Wrote decomposed object to a wrl file... ")
+        return
+
     def decompose_obj(self, graph, de_obj_path):
         """Object skeleton ==> decompsed cubes 
            *Important! all sizes are shrinked to 1/2 in bin simulator
@@ -61,30 +138,43 @@ class TangleObjSke(object):
         node = np.array(graph["node"])
         edge = graph["edge"]
 
+        cubes_size = []
+        cubes_position = []
+        cubes_quat = []
+
         fp = open(de_obj_path, 'wt')
 
         if graph["section"]["shape"] == "circle":
             radius = graph["section"]["size"] 
             cx = radius
             cz = radius
+            
         elif graph["section"]["shape"] == "rectangle":
-            cx, cz = graph["section"]["size"] 
+            cx, cz = graph["section"]["size"]
+
+        cx, cz = cx/2, cz/2
+
         # fit cube to each edge
         cube_num = len(edge)
+
         # center of mass
         com = self.calc_center_of_mass(graph)
         
-        
         print(" ".join(str(c_) for c_ in com), file=fp)
         print(f"{cube_num}",file=fp)
+
         cube_primitive = [0,1,0]
         cube_quat_primitive = [0,0,0,1]
 
+        vertices = []
         for (i,j) in edge:
+            vertex = []
             # 1. pre cube size
-            cy = calc_2points_distance(node[i], node[j])
+            cy = calc_2points_distance(node[i], node[j])/2
             # 2. cube position
             cp = (node[i]+node[j])/2
+            # TODO: calculate vertices
+
             # 3. cube rotation
             angle_ =  calc_2vectors_angle(cube_primitive, np.abs(node[j] - node[i]))
             if angle_== 0:
@@ -93,8 +183,28 @@ class TangleObjSke(object):
             else:
                 c_rot_mat = calc_2vectors_rot_mat(cube_primitive, (node[j] - node[i]))
                 cq = mat2quat(c_rot_mat)
+
+            ncx, ncy, ncz = np.dot(c_rot_mat,[cx, cy, cz])
+
+            vertex.append([cp[0]-ncx, cp[1]+ncy, cp[2]-ncz])
+            vertex.append([cp[0]-ncx, cp[1]-ncy, cp[2]-ncz])
+            vertex.append([cp[0]+ncx, cp[1]-ncy, cp[2]-ncz])
+            vertex.append([cp[0]+ncx, cp[1]+ncy, cp[2]-ncz])
+            
+            vertex.append([cp[0]-ncx, cp[1]+ncy, cp[2]+ncz])
+            vertex.append([cp[0]-ncx, cp[1]-ncy, cp[2]+ncz])
+            vertex.append([cp[0]+ncx, cp[1]-ncy, cp[2]+ncz])
+            vertex.append([cp[0]+ncx, cp[1]+ncy, cp[2]+ncz])
+
+
+            vertices.append(vertex)
+            # save the data
+            # cubes_size.append([cx,cy,cz])
+            # cubes_position.append(cp)
+            # cubes_quat.append(cq)
+
             # write to file
-            print(f"{cx/2} {cy/2} {cz/2}",file=fp)
+            print(f"{cx} {cy} {cz}",file=fp)
             print(" ".join(str(p_) for p_ in cp), file=fp)
             print(f"{cq[3]} {cq[0]} {cq[1]} {cq[2]}", file=fp)
             print(" ".join(str(r_) for r_ in c_rot_mat[0]), file=fp)
@@ -103,16 +213,19 @@ class TangleObjSke(object):
 
         fp.close()
         main_proc_print("Wrote collision file... ")
-        
+
+        return vertices
+
 
 def main():
-    shape = "scylindertwist"
+    shape = "scylinder"
 
     # write_path = f"D:\\code\\myrobot\\objmodel\\skeleton_{shape}.json"
     # collision_path = f"D:\\code\\myrobot\\objmodel\\collision_{shape}.txt"
 
     write_path = f"./objmodel\\skeleton_{shape}.json"
     collision_path = f"./objmodel\\collision_{shape}.txt"
+    wrl_path = f"./objmodel\\cube_{shape}.wrl"
 
     tok = TangleObjSke()
     obj_ske = tok.load_obj(write_path)
@@ -120,11 +233,19 @@ def main():
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     tok.draw_obj_skeleton(obj_ske, ax, "green")
+
     center = tok.calc_center_of_mass(obj_ske)
     ax.scatter(center[0], center[1], center[2], color='red')
-    plt.show()
 
-    tok.decompose_obj(obj_ske, collision_path)
+    vertices=tok.decompose_obj(obj_ske, collision_path)
+
+    # tok.write_decomposed_obj(vertices, wrl_path)
+    
+    for v in vertices:
+        tok.draw_node(v, ax, 'blue', alpha=0.5)
+
+    # optional: write vertices to the vertex_*.txt
+    plt.show()
 
 if __name__ == "__main__":
     
